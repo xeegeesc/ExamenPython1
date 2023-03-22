741320dd8d276646a6420b4c163c573df1ca03ae
Javier Egea <x.egea.es@gmail.com>
2023-03-22 10:31:26 +0100

    checkpoint_2

--- functions.py
+++ functions.py
@@ -1,10 +1,21 @@
 import csv
 
 def read_data(nombreFichero):
+    diccionario = {}
+    indices = ["type","fixed" "acidity","volatile" "acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","quality"]
+    with open(nombreFichero, 'r') as file:
+        reader = csv.reader(file)
+        j=0
+        for row in reader:
+            i=0
+            for dato in row:
+                diccionario["dato"+j]={indices[i]+":"+dato}
+                i+=1
+            j+=1
 
-    f = open(nombreFichero, mode="rt", encoding="utf-8")
-    reader = csv.reader(file, delimiter = "\t")
-    data = f.read()
-    f.close()
+    print(diccionario)
+def __main__():
+    read_data('winequality.csv')
 
-     
+if __name__ == '__main__':
+    __main__()
